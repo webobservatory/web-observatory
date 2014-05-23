865a35643994bfc801ca0f30b2161ad8a19080e6
Xin Wang <xinxinbird@gmail.com>
2014-05-23 23:50:52 +0100

    update to express 4.x

--- app.js
+++ app.js
@@ -8,12 +8,19 @@ var express = require('express'),
     path = require('path'),
     mongoose = require('mongoose'),
     passport = require("passport"),
-    logger = require('./app/util/logger'),
-    flash = require("connect-flash");
+    flash = require("connect-flash"),
+    //express 4.0 middlewares
+    morganLogger = require('morgan'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    favicon = require('static-favicon'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    logging = require('./app/controllers/middleware/logging');
 
 var env = process.env.NODE_ENV || 'development',
     config = require('./config/config')[env];
-
 
 mongoose.connect(config.db);
 
@@ -30,52 +37,30 @@ require('./config/passport')(passport, config);
 
 var app = express();
 
-var logging = function(req, res, next) {
-    if (0 === req.path.indexOf('/css') * req.path.indexOf('/js') * req.path.indexOf('/fonts') * req.path.indexOf('/images')) return next();
-
-    var _req = {};
-
-    _req.port = req.port;
-    _req.host = req.host;
-    _req.accept = req.accept;
-    _req.authorization = req.authorization;
-    _req['user-agent'] = req['user-agent'];
-    _req['content-type'] = req['content-type'];
-    _req.pathname = req.pathname;
-    _req.path = req.path;
-    _req.query = req.query;
-    _req.method = req.method;
-
-    logger.info(_req);
-    next();
-};
-
 app.locals.moment = require('moment');
-app.configure(function() {
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/app/views');
-    app.engine('jade', require('jade').__express);
-    app.set('view engine', 'jade');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.cookieParser());
-    app.use(express.bodyParser());
-    app.use(express.session({
-        secret: 'keyboard cat'
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(passport.authenticate('remember-me'));
-    app.use(express.methodOverride());
-    app.use(flash());
-    app.use(logging);
-    app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/app/views');
+app.engine('jade', require('jade').__express);
+app.set('view engine', 'jade');
+//app.use(favicon);
+app.use(morganLogger('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({
+    secret: 'keyboard cat'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.authenticate('remember-me'));
+app.use(methodOverride());
+app.use(flash());
+app.use(logging);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure('development', function() {
-    app.use(express.errorHandler());
-});
+require('./config/routes')(app, passport);
+
+var env = process.env.NODE_ENV || 'development';
+if ('development' === env) app.use(errorHandler());
 
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -101,9 +86,8 @@ app.use(function(req, res, next) {
     res.type('txt').send('Not found');
 });
 
-require('./config/routes')(app, passport);
 
-http.createServer(app).listen(app.get('port'), function() {
+app.listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port'));
 });
 
