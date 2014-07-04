753598e771340157f5b8096b2f8da25b28536632
Xin Wang <xinxinbird@gmail.com>
2014-07-05 00:44:46 +0100

    update catalog view & add deep linking

--- public/js/catalogue.jade.js
+++ public/js/catalogue.jade.js
@@ -8,11 +8,13 @@ function editEtry(etryId, acc, vis) {
 $(document).ready(function() {
 
     $('.tp').tooltip();
+    /*
     $('#display').dataTable({
         "sPaginationType": "bs_normal"
     });
+    */
 
-/*
+    /*
     $('.edit').bind('click', function(e) {
         editEtry($(this).attr('eid'), $(this).attr('acc'), $(this).attr('vis'));
         e.preventDefault();
@@ -21,16 +23,31 @@ $(document).ready(function() {
     $('#submit').bind('click', function() {
         $('#editForm').submit();
     });
+
     $('.disabled').bind('click', function(event) {
         $('.alert.alert-info').show();
         $('.alert.alert-info').fadeIn('fast');
         event.preventDefault();
     });
     */
+
     //datatable bt3
     var search_input = $('.dataTables_filter input[type=text]');
     search_input.attr('placeholder', 'Search').addClass('form-control input-small');
     $('.dataTables_length select').addClass('form-control input-small');
     //$('.dataTables_length label').addClass('control-label');
 
+    //deep linking
+    $.address.strict(false);
+    $.address.change(function(event) {
+        console.log(event);
+        var id = event.value;
+        if (id) {
+            $('#details').load('/wo/' + id);
+            $('#display').addClass('col-md-6');
+        } else {
+            $('#details').html('');
+            $('#display').removeClass('col-md-6');
+        }
+    });
 });
