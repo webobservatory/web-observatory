0e2ada4cf598ccc8c7db0d169c05da1177c5b1d4
xgfd <xinxinbird@gmail.com>
2014-07-25 14:06:26 +0100

    fix

--- app.js
+++ app.js
@@ -109,4 +109,4 @@ server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
-exports.app = app; //for vhost
+exports.app = app;//for vhost
