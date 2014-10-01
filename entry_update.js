b9b2f34b26a6793dd4e333094ae617bc185c2742
xgfd <xinxinbird@gmail.com>
2014-10-01 17:52:18 +0100

    entry view fix, entry_update fix

--- entry_update.js
+++ entry_update.js
@@ -3,58 +3,65 @@ var mongoose = require('mongoose');
 
 var mongo_url = process.argv[2];
 
+console.log(mongo_url);
+
 mongoose.connect(mongo_url);
 
 var User = require('./app/models/user'),
     Entry = require('./app/models/entry');
 
-User.find({}, function (err, users) {
+User.find({}).populate('own').populate('visible').populate('readable').exec(function (err, users) {
     if (err) {
         return console.log(err);
     }
 
     users.forEach(function (user) {
-        user.populate('own').populate('visible').populate('readable').exec(function (err, user) {
 
-            if (err) {
-                return console.log(err);
-            }
-
-            user.visible.forEach(function (entry) {
-                entry.canView.push(user.email);
-                entry.save(function (err) {
-                    if (err) {
-                        console.log('add view');
-                        console.log(err);
-                    }
-                });
+        console.log('start adding view');
+        if (!user.visible) {
+            user.visible = [];
+        }
+        user.visible.forEach(function (entry) {
+            entry.canView.push(user.email);
+            entry.save(function (err) {
+                if (err) {
+                    console.log('add view');
+                    console.log(err);
+                }
             });
+        });
 
-            user.readable.forEach(function (entry) {
-                entry.canAccess.push(user.email);
-                entry.save(function (err) {
-                    if (err) {
-                        console.log('add access');
-                        console.log(err);
-                    }
-                });
+        console.log('start adding access');
+        if (!user.readable) {
+            user.readable = [];
+        }
+        user.readable.forEach(function (entry) {
+            entry.canAccess.push(user.email);
+            entry.save(function (err) {
+                if (err) {
+                    console.log('add access');
+                    console.log(err);
+                }
             });
+        });
 
-            user.own.forEach(function (entry) {
-                entry.canView.push(user.email);
-                entry.canAccess.push(user.email);
-                entry.save(function (err) {
+        console.log('start adding own');
+        if (!user.own) {
+            user.own = [];
+        }
+        user.own.forEach(function (entry) {
+            entry.canView.push(user.email);
+            entry.canAccess.push(user.email);
+            entry.save(function (err) {
 
-                    if (err) {
-                        console.log('add own');
-                        console.log(err);
-                    }
-                });
+                if (err) {
+                    console.log('add own');
+                    console.log(err);
+                }
             });
-
-            console.log('finished');
         });
     });
+    console.log('finished');
 });
 
 
