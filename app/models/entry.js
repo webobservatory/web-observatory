43eeeb5cadc4b24c3e987dc75d946a06ee9ab6c2
xgfd <xinxinbird@gmail.com>
2014-10-02 12:18:24 +0100

    auto index entry fileds

--- app/models/entry.js
+++ app/models/entry.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose'),
 
 var EntrySchema = mongoose.Schema({
     url: String,
-    name: String,
+    name: {type: String, index: true},
     type: String, //dataset? visualisation? etc.
-    querytype: String, //query interface tyep. e.g. sparql, mysql, mongodb
-    creator: String, //creator of this entry
+    querytype: {type: String, index: true}, //query interface tyep. e.g. sparql, mysql, mongodb
+    creator: {type: String, index: true}, //creator of this entry
     publisher: String, //email of the publisher. use email in case user._id goes wrong it's easier to recover
-    publisher_name: String, //name of the publisher
+    publisher_name: {type: String, index: true}, //name of the publisher
     pubdate: {
         type: Date,
         default: Date.now
@@ -30,9 +30,9 @@ var EntrySchema = mongoose.Schema({
     },
     related: String, //related sources
     git: String, //github url if applicable
-    lice: String, //licence
-    kw: [String], //keywords
-    des: String //description
+    lice: {type: String, index: true}, //licence
+    kw: {type: [String], index: true}, //keywords
+    des: {type: String, index: true} //description
 });
 var Entry = mongoose.model('Entry', EntrySchema);
 module.exports = Entry;
