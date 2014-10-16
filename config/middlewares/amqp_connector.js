99c65b38c80f8720b5ae575a8123dbd4b39d3100
xgfd <xinxinbird@gmail.com>
2014-10-16 12:18:58 +0100

    simple pooling for amqp connections

--- config/middlewares/amqp_connector.js
+++ config/middlewares/amqp_connector.js
@@ -2,14 +2,15 @@
  * Created by Xin on 15/10/2014.
  */
 //
-var amqp = require('amqplib/callback_api');
+var amqp = require('amqplib/callback_api'),
+    connections = {};
 
 function logMessage(cb) {
     "use strict";
     return function (msg) {
         var result;
         if (msg) {
-            result = msg.content.toString();
+            result = msg.content;
             console.log(" [x] '%s'", result);
             cb(null, result);
         }
@@ -20,7 +21,7 @@ function on_channel_open(ex, cb) {
     "use strict";
     return function (err, ch) {
         if (err) {
-            cb(err);
+            return cb(err);
         }
         ch.assertQueue('', {exclusive: true}, function (err, ok) {
             if (err) {
@@ -30,30 +31,35 @@ function on_channel_open(ex, cb) {
             ch.bindQueue(q, ex, '');
             ch.consume(q, logMessage(cb), {noAck: true}, function (err) {
                 if (err) {
-                    cb(err);
+                    return cb(err);
                 }
-                cb(null, ex + ' waiting for messages');
+                cb(null, ex + ' waiting for messages', ch);
             });
         });
     };
 }
 
-
-/*amqp.connect('amqp://test:admin@app-001.ecs.soton.ac.uk', function (err, conn) {
- "use strict";
- if (err) {
- bail(err);
- }
- conn.createChannel(on_channel_open('wikipedia_hose'));
- });*/
-
 //TODO add pooling for connections & channels
 module.exports.getStream = function (opts, cb) {
     "use strict";
-    amqp.connect(opts.url, function (err, conn) {
-        if (err) {
-            cb(err);
-        }
+    var url = opts.url, conn = connections[url];
+    if (conn) {
         conn.createChannel(on_channel_open(opts.ex, cb));
-    });
+    } else {
+        amqp.connect(url, function (err, conn) {
+            if (err) {
+                return cb(err);
+            }
+            connections[url] = conn;
+            conn.createChannel(on_channel_open(opts.ex, cb));
+        });
+    }
+};
+
+module.exports.close=function(url, cb){
+    "use strict";
+    var conn = connections[url];
+    if(conn){
+        conn.close(cb);
+    }
 };
