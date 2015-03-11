112148ed99ca1c885ffc21e45d5d92de81a20c52
Xgfd <xinxinbird@gmail.com>
2015-03-11 15:02:40 +0000

    update howto and query interface

--- public/js/sparql.jade.js
+++ public/js/sparql.jade.js
@@ -9,7 +9,6 @@ var namespaces = {
     'http://www.w3.org/1999/02/22-rdf-syntax-ns#': 'rdf',
     'http://xmlns.com/foaf/0.1/': 'foaf',
     'http://purl.org/dc/elements/1.1/': 'dc',
-    'http://dbpedia.org/resource/': '',
     'http://dbpedia.org/property/': 'dbpedia2',
     'http://dbpedia.org/': 'dbpedia',
     'http://www.w3.org/2004/02/skos/core#': 'skos',
