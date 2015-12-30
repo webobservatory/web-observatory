fff0d0245f897f9d91d6a0f14dc1bb76d4bc6701
Xgfd <xgfd@ip-036-024.vpn.soton.ac.uk>
2015-12-30 01:00:53 +0000

    access request, approval/denial and notification

--- client/templates/entries/entry_page.js
+++ client/templates/entries/entry_page.js
@@ -1,9 +1,66 @@
 Template.entryPage.helpers({
     appendHash(str) {
         return '#' + str;
-    }
+    },
 });
 Template.entryPage.rendered = function () {
     $('ul.tabs').tabs();
-    //window.location.href = window.location.hash;
 };
+
+SimpleSchema.messages({acceptTerms: 'You must accept the terms and conditions and privacy policy of this item'});
+
+Template.requestFrom.rendered = function () {
+    $('.collapsible').collapsible({
+        accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+};
+
+Template.requestFrom.helpers({
+    requestSchema(){
+        return new SimpleSchema({
+            name: {type: String, label: 'Name', autoform: {readonly: true}},
+            username: {
+                type: String,
+                label: 'Username',
+                autoform: {readonly: true}
+            },
+            email: {
+                type: String,
+                autoform: {readonly: true}
+            },
+            organisation: {type: String, label: 'Organisation', optional: true},
+            acceptTerms: {
+                type: Boolean,
+                optional: true,
+                label: 'Accept the license of this item',
+                custom: function () {
+                    if (!this.value) {
+                        return "acceptTerms";
+                    }
+                }
+            }
+        });
+    }
+});
+
+Template.requestFrom.events({
+    'submit form': function (e, template) {
+        e.preventDefault();
+
+        var user = Meteor.user(),
+            $target = $(e.target);
+
+        var initiatorId = user._id,
+            name = user.profile.name || $target.find('[name=name]').val(),
+            username = user.username,
+            email = user.email || $target.find('[name=email]').val(),
+            organisation = $target.find('[name=organisation]').val(),
+            acceptTerms = $target.find('[name=acceptTerms]').prop('checked');
+
+        if (initiatorId && acceptTerms) {
+            Meteor.call('createRequestNotification', username, organisation, initiatorId, template.data.entry, template.data.category.singularName);
+            alert("Request sent");
+        }
+    }
+});
+
--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -14,10 +14,57 @@ Template.notificationItem.helpers({
 });
 
 Template.notificationItem.events({
-    'click a': function () {
-        Notifications.update(this._id, {$set: {read: true}});
+    //make sure the listened doms are disjointed
+    //if one listener remove the dom and the event propagates up will cause Uncaught Error: Must be attached
+    'click .card-content': function (e, template) {
+        //only dismiss notifications that do not need an action
+        if (!template.data.actions) {
+            Notifications.update(template.data._id, {$set: {read: true}});
+        }
+    },
+    'click .card-action a': function (e, template) {
+        //e.preventDefault();
+        var $target = $(e.target),
+            action = $target.attr('notification-action');
+
+        //dismiss notification only if the action succeed
+        if (actions[action](template.data)) {
+            Notifications.update(template.data._id, {$set: {read: true}});
+        }
     }
 });
+
+var actions = {
+    Allow: function (notification) {
+        //console.log(notification);
+        var category;
+
+        if (notification.category === 'dataset') {
+            category = Datasets;
+        }
+
+        if (notification.category === 'app') {
+            category = Apps;
+        }
+
+        var entry = category.update(notification.entryId, {$addToSet: {contentWhiteList: notification.initiatorId}});
+
+        if (entry) {
+            var path = Router.routes[notification.category + '.page'].path({_id: notification.entryId});
+            var message = '<a href="' + path + '">Access granted</a>';
+            Meteor.call('createNotification', Meteor.userId(), notification.entryId, notification.initiatorId, notification.category, message);
+        }
+
+        return !!entry;
+    },
+    Deny: function (notification) {
+        //console.log(notification);
+        var path = Router.routes[notification.category + '.page'].path({_id: notification.entryId});
+        var message = '<a href="' + path + '">Access request declined</a>';
+        Meteor.call('createNotification', Meteor.userId(), notification.entryId, notification.initiatorId, notification.category, message);
+        return true;
+    }
+};
 
 Template.notifications.rendered = function () {
     $(".dropdown-button").dropdown();
