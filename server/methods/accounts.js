a4fbfb3fa286128e80c29c7c40c6bbad326b5c6e
Xgfd <xgfd@Xgfd-MacBook.local>
2016-01-13 14:09:46 +0000

    refactor

---
+++ server/methods/accounts.js
@@ -0,0 +1,16 @@
+/**
+ * Created by xgfd on 25/12/2015.
+ */
+//Accounts.onCreateUser(function (options, user) {
+//    let profile = options.profile;
+//    if (profile) {
+//        if (profile.isgroup) {
+//            delete profile.isgroup;
+//            user.isGroup = Groups.insert({publisher: user._id, name: profile.name});
+//            Roles.removeUserFromRoles( user._id, ["individual"] );
+//            Roles.addUserToRoles( user._id ,  ["group"] );
+//        }
+//        user.profile = options.profile;
+//    }
+//    return user;
+//});
