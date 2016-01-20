c7809b4e6aa197b3681acdec44175bf72cdd1949
Xgfd <xgfd@Xgfd-MacBook.local>
2016-01-20 00:39:11 +0000

    update osx build; add build script

---
+++ src/build.sh
@@ -0,0 +1,8 @@
+rm ../build/osx/*
+rm ../build/linux_64/*
+
+meteor build ../build/linux_64/ --architecture=os.linux.x86_64
+meteor build ../build/osx/ --architecture=os.osx.x86_64
+
+mv ../build/linux_64/src.tar.gz ../build/linux_64/wo.tar.gz
+mv ../build/osx/src.tar.gz ../build/osx/wo.tar.gz
