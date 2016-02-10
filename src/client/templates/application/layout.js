f5eb14c47dbb01161b66dcb96e16e80d8baf30c0
Eugene Siow <kyo116@gmail.com>
2016-02-10 15:29:16 +0000

    Add menu

--- src/client/templates/application/layout.js
+++ src/client/templates/application/layout.js
@@ -44,6 +44,48 @@ Template.layout.rendered = function() {
             $('.navbar-toggle:visible').click();
         }
     });
+
+    /* Off-Canvas Menu */
+    (function () {
+
+        var bodyEl = document.body,
+            content = document.querySelector('.content-wrap'),
+            openbtn = document.getElementById('open-button'),
+            closebtn = document.getElementById('close-button'),
+            isOpen = false;
+
+        function init() {
+            initEvents();
+        }
+
+        function initEvents() {
+            openbtn.addEventListener('click', toggleMenu);
+            if (closebtn) {
+                closebtn.addEventListener('click', toggleMenu);
+            }
+
+            // close the menu element if the target it´s not the menu element or one of its descendants..
+            content.addEventListener('click', function (ev) {
+                var target = ev.target;
+                if (isOpen && target !== openbtn) {
+                    toggleMenu();
+                }
+            });
+        }
+
+        function toggleMenu() {
+            if (isOpen) {
+                classie.remove(bodyEl, 'show-menu');
+            }
+            else {
+                classie.add(bodyEl, 'show-menu');
+            }
+            isOpen = !isOpen;
+        }
+
+        init();
+
+    })();
 };
 
 Template.layout.events({
