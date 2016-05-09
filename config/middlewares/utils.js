dbade23e56dd21db6ca9077d4df6a0cb12fc2018
Xgfd <xinxinbird@gmail.com>
2016-05-09 13:34:29 +0100

    comply to voice style

--- config/middlewares/utils.js
+++ config/middlewares/utils.js
@@ -2,14 +2,15 @@
  * Created by xgfd on 11/11/14.
  */
 exports.forceSSL = function (req, res, next) {
-    'use strict';
-    if (!req.secure) {
-        var sslPort = req.app.get('httpsPort');
-        console.log(req.hostname);
-        res.redirect('https://' + req.hostname + ':' + sslPort + req.url);
-    } else {
-        next();
-    }
+    next();
+    // 'use strict';
+    // if (!req.secure) {
+    //     var sslPort = req.app.get('httpsPort');
+    //     console.log(req.hostname);
+    //     res.redirect('https://' + req.hostname + ':' + sslPort + req.url);
+    // } else {
+    //     next();
+    // }
 };
 
 exports.noneSSL = function (req, res, next) {
