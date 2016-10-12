9b087eefc5340fcd3f7ffcc0ca7ae908829709dd
Xgfd <xinxinbird@gmail.com>
2016-10-12 17:10:14 +0100

    editor's choice as a section

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
-Template.home.onRendered(function() {
-    $('a.page-scroll').bind('click', function(event) {
+Template.home.onRendered(function () {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
@@ -8,3 +8,24 @@ Template.home.onRendered(function() {
     });
 });
 
+Template.home.helpers({
+    editorChoice(tmpData) {
+        let coll = tmpData.category;
+        let oldEntries = tmpData.entries.fetch();
+        let adminIds = Meteor.users.find({roles: 'admin'}).map(admin=>admin._id);
+        tmpData.entries = coll.find({upvoters: {$in: adminIds}});
+        return tmpData;
+    }
+});
+
+
+function isEditorChoice(entry) {
+    let adminIds = Meteor.users.find({roles: 'admin'}).map(admin=>admin._id);
+    let upvoters = entry.upvoters;
+
+    let inter = _.intersection(adminIds, upvoters);
+
+    let editorChoice = inter.length !== 0;
+
+    return editorChoice;
+}
